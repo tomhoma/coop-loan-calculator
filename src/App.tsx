188ee8e52de1bcb262bd4dc6ddb1f185ed258971
Theera Suwanchatree <theera799@gmail.com>
2023-08-20 10:16:19 +0700

    add mui

--- src/App.tsx
+++ src/App.tsx
@@ -2,57 +2,39 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 
 
-const useStyles = makeStyles({
-  root: {
-    minWidth: 275,
-  },
-  bullet: {
-    display: 'inline-block',
-    margin: '0 2px',
-    transform: 'scale(0.8)',
-  },
-  title: {
-    fontSize: 14,
-  },
-  pos: {
-    marginBottom: 12,
-  },
-});
+const bull = (
+  <Box
+    component="span"
+    sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.5)' }}
+  >
+    +
+  </Box>
+);
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-    <Card className={classes.root}>
-      <CardContent>
-        <Typography className={classes.title} color="textSecondary" gutterBottom>
-          Word of the Day
-        </Typography>
-        <Typography variant="h5" component="h2">
-          be{bull}nev{bull}o{bull}lent
-        </Typography>
-        <Typography className={classes.pos} color="textSecondary">
-          adjective
-        </Typography>
-        <Typography variant="body2" component="p">
-          well meaning and kindly.
-          <br />
-          {'"a benevolent smile"'}
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small">Learn More</Button>
-      </CardActions>
-    </Card>
+const card = (
+  <>
+  
+    <CardContent>
+       <Typography sx={{ fontSize: 14 }} color="text.primary" gutterBottom>
+        Word of the Day
+      </Typography>
+      <Typography variant="h1" component="div">
+        Vite{bull}React
+      </Typography>
+      {/* <Typography sx={{ mb: 1.5 }} color="text.secondary">
+        adjective
+      </Typography> */}
+      <Typography variant="body2">
+  
+      </Typography> 
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo react" alt="Vite logo" />
@@ -61,7 +43,24 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+    </CardContent>
+    <CardActions>
+      <Button size="small">Learn More</Button>
+    </CardActions>
+  </>
+);
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <Box sx={{ minWidth: 275 }}>
+      <Card variant="outlined">{card}</Card>
+      </Box>
+    
+ 
+
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -73,11 +72,8 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+    
     </>
   )
 }
-export default function SimpleCard() {
-  const classes = useStyles();
-  const bull = <span className={classes.bullet}>•</span>;
-  App
-}
+export default App
